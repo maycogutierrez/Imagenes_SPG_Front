1a685071df717fa90c0cc75782cfd5de17996e88
Mayco Austin Gutierrez <maycogutix43@gmail.com>
2026-09-07 20:15:45 -0300

    Refactor code structure for improved readability and maintainability

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,14 +3,14 @@ import "../css/style.css";
 
 const Footer = () => (
   <footer className="footer">
-    © 2025 NewLink Ingeniería. Todos los derechos reservados. | Desarrollado por NewLink Ingeniería |{" "}
+    © 2026 Mayco Gutierrez. Todos los derechos reservados. | Desarrollado por Mayco Gutierrez |{" "}
     <a
-      href="https://wa.me/543572520716"
+      /* href="https://wa.me/------------" */
       target="_blank"
       rel="noopener noreferrer"
       style={{ color: "#25D366", textDecoration: "none", fontWeight: "bold" }}
     >
-      Tel: 03572 520716
+      Tel: ------------
     </a>
   </footer>
 );
